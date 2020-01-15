e5bf4300c592d45bbe22ad38c05b8eebc111563e
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-01-15 16:55:05 +0100

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,14 +1,22 @@
+--Author : Thomas Grossmann / Mounir Fiaux
+--Database : SiteJeux_BD_TGN_MFX
+--Date : 15.01.2020
+
 use master
 go
 
+--If the database exists it drops it and create the new one--
 IF db_id('SiteJeuxBD_TGN_MFX') IS NOT NULL
 BEGIN
 	drop database SiteJeuxBD_TGN_MFX
 END
 
+--Creation of the database
 CREATE DATABASE SiteJeuxBD_TGN_MFX
 	ON PRIMARY
+	--Properties of the database's file
 	( NAME = 'SiteJeuxBD_TGN_MFX', FILENAME = 'C:\TGN_MFX\SiteJeuxBD_TGN_MFX.mdf', SIZE = 20480KB , MAXSIZE = 51200KB, FILEGROWTH = 1024KB )
+	--Properties of the database's logs file
 	LOG ON
 	( NAME = 'SIteJeuxBD_TGN_MFX_log', FILENAME = 'C:\TGN_MFX\SiteJeuxBD_TGN_MFX.ldf', SIZE = 10240KB , MAXSIZE = 20480KB , FILEGROWTH = 1024KB)
 go
@@ -16,13 +24,15 @@ go
 use SiteJeuxBD_TGN_MFX
 go
 
+--Creation of the table accessories with type, price, name
 create table accessories (
 	accessoriesID int NOT NULL PRIMARY KEY,
 	type varchar(50),
-	price float,
+	price float CHECK(price>=0),
 	name varchar(50)
 )
 
+--Creation of the table customers with lastname, firstname, nickname, mail, phoneNumber
 create table customers (
 	customersID int NOT NULL PRIMARY KEY,
 	lastname varchar(45),
@@ -32,30 +42,27 @@ create table customers (
 	phoneNumber int
 )
 
+--Creation of the table develpments with cost, duration
 create table developments (
 	developmentsID int NOT NULL PRIMARY KEY,
 	cost int,
 	duration datetime
 )
 
-create table developers (
-	developersID int NOT NULL PRIMARY KEY,
-	name varchar(45),
-	creation date,
-	totalGames int
-)
-
+----Creation of the table types_providers with name
 create table types_providers (
 	types_providersID int NOT NULL PRIMARY KEY,
 	name varchar(45)
 )
 
+--Creation of the table platforms with name, type
 create table platforms (
 	platformsID int NOT NULL PRIMARY KEY,
 	name varchar(45),
 	type varchar(45)
 )
 
+--Creation of the table reductions with name, pourcentage, deadline, foreign key to customersID
 create table reductions (
 	reductionsID int NOT NULL PRIMARY KEY,
 	name varchar(45),
@@ -64,6 +71,7 @@ create table reductions (
 	customers_id int FOREIGN KEY REFERENCES customers(customersID)
 )
 
+--Creation of the table payments with type, foreign key to 
 create table payments (
 	paymentsID int NOT NULL PRIMARY KEY,
 	type varchar(45),
@@ -81,7 +89,7 @@ create table orders (
 create table games (
 	gamesID int NOT NULL PRIMARY KEY,
 	name varchar(45),
-	price float,
+	price float CHECK(price>=0),
 	releaseDate date,
 	developments_id int FOREIGN KEY REFERENCES developments(developmentsID)
 )
@@ -102,4 +110,9 @@ create table providers (
 	creationDate date,
 	nbGames int,
 	types_providers_id int FOREIGN KEY REFERENCES types_providers(types_providersID)
+)
+
+create table providers_has_games (
+	providers_id int FOREIGN KEY REFERENCES providers(providersID),
+	games_id int FOREIGN KEY REFERENCES games(gamesID)
 )
